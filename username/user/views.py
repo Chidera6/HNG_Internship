4385e80b9756af28be5901eb33c339c250ff07ce
chidera6 <dictasteil@gmail.com>
2022-10-28 18:44:18 -0700

    turned the whole project to a simple json response

--- username/user/views.py
+++ username/user/views.py
@@ -1,25 +1,10 @@
-from email.mime import application
-from django.shortcuts import render
-from .models import User
-from .serializers import UserSerializer
-from rest_framework.parsers import JSONParser
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
-@csrf_exempt
+
 def index(request):
-    if request.method == 'GET':
-        all_user = User.objects.all()
-        serializer = UserSerializer(all_user,many=True)
-        return JsonResponse(serializer.data,safe=False)
-        
-
-    elif request.method == 'POST':
-        user_to_get = JSONParser().parse(request)
-        serializer = UserSerializer(data = user_to_get)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data,status=201)
-        return  JsonResponse(serializer.errors,status=400) 
-
+    return JsonResponse({"slackUsername": "Chidera Onumajuru",
+        "backend": True,
+        "age": 26,
+        "bio": "I am a Backend Developer,proficient in the use of Python,Flask framework,Postgresql and Django framework."})
