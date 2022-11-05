f7faeb1355bf702b8269e5fb91165311e0fc19b5
chidera6 <dictasteil@gmail.com>
2022-11-04 19:29:29 -0700

    added a post request

--- username/user/models.py
+++
@@ -1,5 +0,0 @@
-from email.policy import default
-from unittest.mock import DEFAULT
-from django.db import models
-
-# Create your models here.
--- username/user/views.py
+++ username/user/views.py
@@ -1,10 +1,26 @@
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
+from json import loads
+from django.views.decorators.csrf import csrf_exempt
 
-
-# Create your views here.
-
-def index(request):
+"""
+def index():
     return JsonResponse({"slackUsername": "Chidera Onumajuru",
         "backend": True,
         "age": 26,
         "bio": "I am a Backend Developer,proficient in the use of Python,Flask framework,Postgresql and Django framework."})
+"""
+@csrf_exempt
+def calculate(request):
+    if request.method == "POST":
+        new_data = loads(request.body.decode())
+        if "add" in new_data["operation_type"] or "addition" in new_data["operation_type"]:
+            result = new_data["x"] + new_data["y"]
+        elif new_data["operation_type"] == "subtraction" or "subtract" in new_data["operation_type"]:
+            result = new_data["x"] - new_data["y"]
+        else:
+            result = new_data['x'] * new_data["y"]   
+        return JsonResponse({
+            'slackusername':'Chidera Onumajuru',
+            'operation_type':new_data["operation_type"],
+            'result':result},safe=False)
+
